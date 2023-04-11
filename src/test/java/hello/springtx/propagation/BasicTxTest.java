1e877876ed37e634681202241ebb8ceaab1e8af1
ardevtect7 <ardevtect7@gmail.com>
2023-04-11 22:52:07 +0900

    스프링 트랜잭션 전파4-전파 예제

--- src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -75,4 +75,22 @@ public class BasicTxTest {
         log.info("트랜잭션 2 롤백");
         txManager.rollback(tx2);
     }
+
+    @DisplayName("내부 Commit")
+    @Test
+    void inner_commit() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction()={}", outer.isNewTransaction());
+
+        // 외부 트랜잭션이 시작하고 있는데, 내부 트랜잭션 추가로 수행
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("inner.isNewTransaction()={}", inner.isNewTransaction());
+        log.info("내부 트랜잭션 커밋");
+        txManager.commit(inner);
+
+        log.info("외부 트랜잭션 커밋");
+        txManager.commit(outer);
+    }
 }
