e72359b80755175ca608bc1f3f3eb3e33ea2ef4b
ardevtect7 <ardevtect7@gmail.com>
2023-04-13 15:58:42 +0900

    트랜잭션 전파 활용2 - 커밋, 롤백

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Slf4j
@@ -25,7 +26,7 @@ class MemberServiceTest {
      * memberRepository - @Transactional:ON
      * logRepository - @Transactional:ON
      */
-    @DisplayName("밖에 트랜잭션이 없는 경우")
+    @DisplayName("서비스 계층에 트랜잭션이 없는 경우 - 성공")
     @Test
     void outerTxOff_success() {
         // Given
@@ -37,5 +38,25 @@ class MemberServiceTest {
         // Then : 모든 데이터가 정상 저장
         assertTrue(memberRepository.find(username).isPresent());
         assertTrue(logRepository.find(username).isPresent());
+    }
+
+    /**
+     * memberService - @Transactional:OFF
+     * memberRepository - @Transactional:ON
+     * logRepository - @Transactional:ON Exception
+     */
+    @DisplayName("서비스 계층에 트랜잭션이 없는 경우 - 예외")
+    @Test
+    void outerTxOff_fail() {
+        // Given
+        String username = "로그예외_outerTxOff_fail";
+
+        // When
+        assertThatThrownBy(() -> memberService.joinV1(username))
+                        .isInstanceOf(RuntimeException.class);
+
+        // Then : 모든 데이터가 정상 저장
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isEmpty());
     }
 }
